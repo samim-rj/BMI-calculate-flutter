3c562e2c074d2b6ae922df3c975b5239024eb7eb
Samim Mondal <samimtapu69@gmail.com>
2020-05-10 11:14:10 +0530

    yaah this is my first commit.

---
+++ lib/constants.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 80.0;
+const kActiveCardColour = Color(0xFF1D1E33);
+const kInactiveCardColour = Color(0xFF111111);
+const kBottomCardColour = Color(0xFFBB1555);
+
+const kIconTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+
+const kNumberTextStyle = TextStyle(
+  fontWeight: FontWeight.w900,
+  fontSize: 50.0,
+);
+
+const kLargeButtonTextStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 50.0,
+);
+
+const kResultTextStyle = TextStyle(
+  color: Color(0xFF24D876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMITextStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 100.0,
+);
+
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+);
